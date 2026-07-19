0bd562698f9aa0cdde496af9ee455b95f9b4d938
RavinaMore1121 <111903966+RavinaMore1121@users.noreply.github.com>
2026-07-19 22:37:58 +0530

    Update Founder component image class for full width

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -1,11 +1,13 @@
 import founder from "../assets/about-agphoto.png";
 function Founder() {
   return (
+    <section className="w-full">
       <img
         src={founder}
         alt="Founder"
-        className="h-auto object-cover"
+        className="w-full h-auto object-cover"
       />
+      </section>
   );
 }
 export default Founder;
